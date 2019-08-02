57ae8e19318900dd4888ef5134d68ca1152ff462
Atsushi <atsushi88885@gmail.com>
2019-08-02 16:33:12 +0800

    プロパティとメソッド作成

--- MangoSix.php
+++ MangoSix.php
@@ -1,5 +1,26 @@
 <?php
 
 // クラスを定義
+// 定義の仕方
+// class クラス名 {} 
 class MangoSix
-{ }
+{
+    // プロパティ
+    // クラス内の変数
+    private $place;
+
+    // メソッド
+    // クラス内の関数
+    public function provide($name)
+    {
+        echo $name;
+    }
+
+    // アクセス修飾子
+    // プロパティ、メソッドがどこからアクセス可能か決める
+    /**
+     * public どこからでも
+     * protected クラス内、または、継承しているクラス
+     * private クラス内のみ
+     */
+}
